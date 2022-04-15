f69e71f7fc6d548179601ca4c94c00561a4d0d4e
Hunanyan2001 <haruthunanyan10@gmail.com>
2022-04-15 18:10:37 +0400

    first commit

---
+++ XandO/Board.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace XandO
+{
+    public class Board
+    {
+        private int Rows;
+        private int Columns;
+        private Label[,] BlockControls;
+        public Board(Grid BaseGrid)
+        {
+            Rows = BaseGrid.RowDefinitions.Count;
+            Columns = BaseGrid.ColumnDefinitions.Count;
+            BlockControls = new Label[Rows, Columns];
+            for (int i = 0; i < Rows; i++)
+            {
+                for (int j = 0; j < Columns; j++)
+                {
+
+                    BlockControls[i, j] = new Label();
+                    BlockControls[i, j].Background = Brushes.Red;
+                    BlockControls[i, j].BorderBrush = Brushes.Green;
+                    BlockControls[i, j].BorderThickness = new Thickness(1, 1, 1, 1);
+                    Grid.SetColumn(BlockControls[i, j], j);
+                    Grid.SetRow(BlockControls[i, j], i);
+                    BaseGrid.Children.Add(BlockControls[i, j]);
+                }
+            }
+        }
+    }
+}
